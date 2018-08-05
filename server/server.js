8c7d41232841b7cf127352292e0f9668488c0614
Vincent L <vincentluoster93@gmail.com>
2018-08-04 23:05:47 -0700

    connected mongo and updated package.json

--- server/server.js
+++ server/server.js
@@ -6,10 +6,29 @@ import template from './../template'
 import devBundle from './devBundle'
 
 const app = express()
-
 // don't use when building for production
 devBundle.compile(app)
 
-
 const CURRENT_WORKING_DIR = process.cwd()
 app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
+
+app.get('/', (req, res) => {
+    res.status(200).send(template())
+})
+
+let port = process.env.PORT || 3000
+app.listen(port, function onStart(err) {
+  if (err) {
+    console.log(err)
+  }
+    console.info('Server started on port %s.', port)
+})
+
+// Database connection
+const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/mernSimpleSetup'
+// connect to server
+MongoClient.connect(url, (err, db) => {
+    console.log("Connected to mongodb server!")
+    db.close()
+
+})
