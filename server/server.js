3466be74490eb935b405569210c227fa7f5a1ef5
Vincent L <vincentluoster93@gmail.com>
2018-08-04 22:42:52 -0700

    update to node server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,15 @@
 import express from 'express'
+import path from 'path'
+import { MongoClient } from 'mongodb'
+import template from './../template'
+// don't use when building for production
+import devBundle from './devBundle'
 
 const app = express()
 
+// don't use when building for production
+devBundle.compile(app)
+
+
+const CURRENT_WORKING_DIR = process.cwd()
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
